c665e8a95b8c94daf094a0d3c4309aca5cf9a91f
fengjinlong <18618166564@163.com>
2019-10-29 17:51:36 +0800

    date

--- test.js
+++ test.js
@@ -1,8 +1,86 @@
-function isFunction (fn) {
-  return Object.prototype.toString.call(fn) === '[object Function]'
-}
-function Promise(fn) {
-  if (!isFunction(fn)) {
-    throw new Error('Promise must accept a function as a parameter')
+const isType = (obj, type) => {
+  // function 
+  if (typeof obj !== 'object') return false;
+  const typeString = Object.prototype.toString.call(obj);
+  let flag;
+  switch (type) {
+    case 'Array':
+      flag = typeString === '[object Array]';
+      break;
+    case 'Date':
+      flag = typeString === '[object Date]';
+      break;
+    case 'RegExp':
+      flag = typeString === '[object RegExp]';
+      break;
+    default:
+      flag = false;
   }
-}
+  return flag;
+};
+// console.log(isType(new Date(), 'Date'))
+
+
+/**
+* deep clone
+* @param  {[type]} parent object 需要进行克隆的对象
+* @return {[type]}        深克隆后的对象
+*/
+const clone = parent => {
+  // 维护两个储存循环引用的数组
+  const parents = [];
+  const children = [];
+
+  const _clone = parent => {
+    // null
+    if (parent === null) return null;
+    // 原始类型
+    if (typeof parent !== 'object') return parent;
+
+    let child, proto;
+
+    if (isType(parent, 'Array')) {
+      // 对数组做特殊处理
+      child = [];
+    } else if (isType(parent, 'RegExp')) {
+      // 对正则对象做特殊处理
+      // child = new RegExp(parent.source, getRegExp(parent));
+      child = new RegExp(parent.source, parent.flags);
+      console.log(parent.lastIndex)
+      if (parent.lastIndex) child.lastIndex = parent.lastIndex;
+    } else if (isType(parent, 'Date')) {
+      // 对Date对象做特殊处理
+      child = new Date(parent.getTime());
+    } else {
+      // 处理对象原型
+      proto = Object.getPrototypeOf(parent);
+      // 利用Object.create切断原型链
+      child = Object.create(proto);
+    }
+
+    // 处理循环引用
+    const index = parents.indexOf(parent);
+
+    if (index != -1) {
+      // 如果父数组存在本对象,说明之前已经被引用过,直接返回此对象
+      return children[index];
+    }
+    parents.push(parent);
+    children.push(child);
+
+    for (let i in parent) {
+      // 递归
+      child[i] = _clone(parent[i]);
+    }
+
+    return child;
+  };
+  return _clone(parent);
+};
+var regex = new RegExp(/xyz/, 'i');
+regex.lastIndex = 2;
+// var regex = /hello\d/y;
+// var a = [1,2,3]
+var b = clone(regex)
+// a[0] = 4
+console.log(b)
